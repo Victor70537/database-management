3509226b264a04b45482306d6502ec804ddc1cab
RunjingQ <84996752+RunjingQ@users.noreply.github.com>
2023-10-10 13:32:50 -0400

    Uploaded template project from eLC

---
+++ project2/example-project/src/main/java/edu/uga/cs4370/App.java
@@ -0,0 +1,13 @@
+package edu.uga.cs4370;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
